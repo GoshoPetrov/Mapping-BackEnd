69d89bee9f73c1255395c318d86c38d4cd4ab101
GoshoPetrov <yankrustev@gmail.com>
2024-11-09 17:43:05 +0200

    post button

---
+++ backend/Mapping-BackEnd/Controllers/Api.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mapping_BackEnd.Controllers
+{
+    public class Api : Controller
+    {
+        [HttpPost]
+        public IActionResult Map()
+        {
+            var dict = new Dictionary<string, object>();
+
+            dict["coordinates"] = new double[] { 42.6977, 23.3219 };
+            dict["radius"] = 13;
+            dict["message"] = "Sofia";
+
+            return Json(dict);
+        }
+    }
+}

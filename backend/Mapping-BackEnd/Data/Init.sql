23ec07c77a6282708ca3bbea1d0e7614e7716041
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2024-11-27 17:22:19 -0800

    Script for DB

---
+++ backend/Mapping-BackEnd/Data/Init.sql
@@ -0,0 +1,7 @@
+USE PensaClub
+
+DELETE FROM Places
+
+INSERT INTO [dbo].[Places] ([Id], [Caption], [Lat], [Long], [Radius]) VALUES (1, N'Sofia', 42.698494, 23.3198532, 13)
+INSERT INTO [dbo].[Places] ([Id], [Caption], [Lat], [Long], [Radius]) VALUES (2, N'Softuni', 42.6361668, 23.3698051, 18)
+INSERT INTO [dbo].[Places] ([Id], [Caption], [Lat], [Long], [Radius]) VALUES (3, N'Burgas', 42.5266569, 27.287156, 13)
